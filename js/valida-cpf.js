493bfcec3b8b8f9d569b7cacd1eba7f0575ef304
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-29 12:17:56 -0300

    validation cpf

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,5 +1,7 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g,""); //removendo os caracters como - e .
+    validaNumRep(cpf);
+    validaPrimeiroDigito(cpf);
     console.log(validaNumRep(cpf));
 }
 
@@ -22,3 +24,24 @@ function validaNumRep(cpf){
     //pesquisando se o cpf está dentro do array
     return numeroRepetido.includes(cpf)
 }
+
+
+//validando o primeiro digito
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho <9; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador --;
+    }
+    soma = (soma * 10) % 11;
+    console.log(soma);
+
+    if(soma == 10 || soma == 1){
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
